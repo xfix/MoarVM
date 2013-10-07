ef88782e6360893c509a0cb8bf738450eace88e5
diakopter <diakopter@gmail.com>
2013-10-07 08:10:31 -0400

    merge fail... :S

--- src/gc/collect.c
+++ src/gc/collect.c
@@ -70,11 +70,6 @@ void MVM_gc_collect(MVMThreadContext *tc, MVMuint8 what_to_do, MVMuint8 gen) {
     do_worklist(tc, worklist, gen,
         MVM_gc_root_add_tc_roots_to_worklist, "TC objects");
 
-        /* Add current frame to worklist. */
-        MVM_gc_worklist_add_frame(tc, worklist, tc->cur_frame);
-        GCDEBUG_LOG(tc, MVM_GC_DEBUG_COLLECT, "Thread %d run %d : processing %d items from current frame\n", worklist->items);
-        process_worklist(tc, worklist, &wtp, gen);
-
     /* Add temporary roots and process them (these are per-thread). */
     do_worklist(tc, worklist, gen,
         MVM_gc_root_add_temps_to_worklist, "thread temps");
